1a63deedaa796f3e8842aaf6bd8f4ff968fff358
sootspritepsych <sootspritepsych@gmail.com>
2026-07-07 17:28:18 -0500

    Add adult QOTD scheduler to ready event

--- artifacts/faye-bot/src/events/ready.ts
+++ artifacts/faye-bot/src/events/ready.ts
@@ -2,19 +2,25 @@ import { Client, Events, ActivityType } from "discord.js";
 import { loadReminders } from "../lib/reminderScheduler";
 import { startWisdomScheduler } from "../lib/wisdomScheduler";
 import { startWelcomeJourneyScheduler } from "../lib/welcomeJourneyScheduler";
+import { startAdultQotdScheduler } from "../lib/adultQotdScheduler";
 
 export default function registerReadyEvent(client: Client) {
   client.once(Events.ClientReady, async (readyClient) => {
     console.log(`🌿 Faye is awake! Logged in as ${readyClient.user.tag}`);
 
     readyClient.user.setPresence({
-      activities: [{ name: "over the Garden of Harmony 🌸", type: ActivityType.Watching }],
+      activities: [
+        {
+          name: "over the Garden of Harmony 🌸",
+          type: ActivityType.Watching,
+        },
+      ],
       status: "online",
     });
 
     await loadReminders(client);
     await startWisdomScheduler(client);
     await startWelcomeJourneyScheduler(client);
-    
+    await startAdultQotdScheduler(client);
   });
 }
