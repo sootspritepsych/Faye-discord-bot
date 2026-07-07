12444e0f5e7ff8ede9cf0e13fcd78c6020366a16
sootspritepsych <sootspritepsych@gmail.com>
2026-07-07 17:20:23 -0500

    Add adultqotd command to command list

--- artifacts/faye-bot/src/commands/index.ts
+++ artifacts/faye-bot/src/commands/index.ts
@@ -20,6 +20,7 @@ import * as unavaatu from "./unavaatu";
 import { complimentCommand } from "./compliment";
 import * as ticket from "./ticket";
 import * as addwquotes from "./addwquotes";
+import * as adultqotd from "./adultqotd";
 
 export interface Command {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -49,6 +50,7 @@ const commandList: Command[] = [
   unavaatu,
   complimentCommand,
   ticket,
+  adultqotd,
 ];
 
 export const commands = new Collection<string, Command>();
